d65e5d7076aa9e4182c82aac9b360dc983caacef
Justin Furtado <justin.l.furtado@gmail.com>
2017-02-09 23:43:47 -0700

    Reword part of doc + copy to assignments

---
+++ Assignments/Lab05_GeometryShaders/Engine/FrameBuffer.h
@@ -0,0 +1,34 @@
+#ifndef FRAMEBUFFER_H
+#define FRAMEBUFFER_H
+
+#include "ExportHeader.h"
+#include "MyGL.h"
+
+// Justin Furtado
+// 1/30/2017
+// FrameBuffer.h
+// Class to hold framebuffer code
+
+namespace Engine
+{
+	class ENGINE_SHARED FrameBuffer
+	{
+	public:
+		FrameBuffer();
+		~FrameBuffer();
+
+		bool Initialize(int width, int height);
+		void Bind();
+		void UnBind(int viewX, int viewY, int viewWidth, int viewHeight);
+		GLuint * GetTexIdPtr();
+
+	private:
+		int width;
+		int height;
+		GLuint renderTextureId;
+		GLuint fboHandle;
+		GLuint depthBufferId;
+	};
+}
+
+#endif // ifndef FRAMEBUFFER_H
